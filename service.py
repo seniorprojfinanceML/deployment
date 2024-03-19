5b4852f21111357de5f27b2dff9f147ad1e6394b
Krittapasa <kritapassk138@gmail.com>
2024-03-19 15:28:26 +0700

    Change data type of the request body to JSON input: input of the model (required) model: model name registered to the mlflow (defualt is xgr) alias: used alias (default is production) version: model version (only applied when alias is not given)

--- service.py
+++ service.py
@@ -1,5 +1,5 @@
 import bentoml
-from bentoml.io import NumpyNdarray
+from bentoml.io import JSON, NumpyNdarray
 import mlflow
 import numpy as np
 import os
@@ -8,12 +8,23 @@ svc = bentoml.Service("XGBoostService")
 URL = os.environ.get('URL')
 mlflow.set_tracking_uri(URL)
 
-@svc.api(input=NumpyNdarray(), output=NumpyNdarray()) 
-def predict(data: np.ndarray) :
-    model_name = "xgr"
+@svc.api(input=JSON(), output=NumpyNdarray()) 
+def predict_xgboost(data) :
+    if 'input' not in data:
+        raise ValueError("input is not provided")
+    model_name = 'xgr'
     uri = f"models:/{model_name}@production"
+    if 'model' in data:
+        model_name = data['model']
+    if 'alias' in data:
+        uri = f"models:/{model_name}@{data['alias']}"
+    elif 'version' in data:
+        uri = f"models:/{model_name}/{data['version']}"
     model = mlflow.xgboost.load_model(uri)
-    prediction = model.predict(data)
-    # model = mlflow.pyfunc.load_model(uri)
-    # prediction = model.predict(data)
+    prediction = model.predict(data["input"])
+    # model_name = "xgr"
+    
+    # # model = mlflow.pyfunc.load_model(uri)
+    # # prediction = model.predict(data)
     return prediction
+    # return np.zeros(shape=2)
